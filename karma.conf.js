0402e1b89bd5f3e85daa07b7741882c65eb242cb
valentinbancel <valentin.bancel@orange.fr>
2023-11-29 16:28:34 +0100

    fix npm run cript

--- karma.conf.js
+++ karma.conf.js
@@ -4,12 +4,20 @@ const puppeteer = require('puppeteer');
 module.exports = function(config) {
   config.set({
     // ... other Karma configuration options
-    browsers: ['CustomChromeHeadless'],
+    browsers: ['Puppeteer'],
     customLaunchers: {
-      CustomChromeHeadless: {
+      Puppeteer: {
         base: 'ChromeHeadless',
-        flags: ['--no-sandbox', '--disable-setuid-sandbox']
-      }
+        flags: [
+          '--no-sandbox',
+          '--disable-setuid-sandbox',
+          '--disable-gpu',
+          '--disable-software-rasterizer',
+          '--disable-dev-shm-usage',
+          '--headless',
+          '--remote-debugging-port=9222',
+        ],
+      },
     },
     // ... other configuration options
   });
