c95bf760e3a151555152d9daf80d6ef5684d794d
valentinbancel <valentin.bancel@orange.fr>
2023-12-05 18:59:19 +0100

    clean router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-	// { path: '', redirectTo: '/home', pathMatch: 'full' },
-	// { path: 'home', component: HomeComponent },
-	// { path: 'about', component: AboutComponent },
-	// { path: 'project', component: ProjectComponent },
-];
+const routes: Routes = [];
 
 @NgModule({
 	imports: [RouterModule.forRoot(routes)],
