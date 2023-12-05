139770b3230c141c94425741994a49e42063816e
valentinbancel <valentin.bancel@orange.fr>
2023-12-05 18:52:25 +0100

    change indent to tab 4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+	// { path: '', redirectTo: '/home', pathMatch: 'full' },
+	// { path: 'home', component: HomeComponent },
+	// { path: 'about', component: AboutComponent },
+	// { path: 'project', component: ProjectComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
 })
 export class AppRoutingModule { }
