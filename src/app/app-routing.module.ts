795b0234d09f3e1ba7b9a7788c7273e94c320714
valentinbancel <valentin.bancel@orange.fr>
2023-12-14 00:24:44 +0100

    create many route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: 'header',
+		component: HeaderComponent
+	},
+	{
+		path: 'home',
+		component: HomePageComponent
+	},
+	{
+		path: '',
+		redirectTo: '/home',
+		pathMatch: 'full'
+	}
+];
 
 @NgModule({
 	imports: [RouterModule.forRoot(routes)],
 	exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
