44851491ccc671e9437cc2755e1c726e2e8468e9
valentinbancel <valentin.bancel@orange.fr>
2023-12-14 00:27:08 +0100

    add(home): create home component

---
+++ src/app/home-page/home-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+
+@Component({
+  selector: 'app-home-page',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.scss']
+})
+export class HomePageComponent {
+
+}
