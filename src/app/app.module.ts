8d8be116fe28217aecb4f9e21fb1ec44ccc0bbdc
valentinbancel <valentin.bancel@orange.fr>
2023-12-14 00:27:45 +0100

    add(component): define all new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
 	declarations: [
-		AppComponent
+		AppComponent,
+		HeaderComponent,
+		FooterComponent,
+		HomePageComponent
 	],
 	imports: [
 		BrowserModule,
-		AppRoutingModule
+		AppRoutingModule,
 	],
 	providers: [],
-	bootstrap: [AppComponent]
+	bootstrap: [
+		AppComponent,
+	]
 })
 export class AppModule { }
