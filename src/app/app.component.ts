6e20896b70a8421515e65f4864913306cb0bf60f
valentinbancel <valentin.bancel@orange.fr>
2023-12-16 23:18:28 +0100

    add(footer): add footer code in app ts for passed test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
 	selector: 'app-root',
