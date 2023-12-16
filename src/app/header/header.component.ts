0378b9086930df2df5600155ccd336775a5b451d
valentinbancel <valentin.bancel@orange.fr>
2023-12-16 23:13:36 +0100

    add(header): code header version 1

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
 	selector: 'app-header',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
 	styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+	home_classe: string = 'button-text col-start-10';
+	about_me_classe: string = 'button-text col-start-11';
+	project_classe: string = 'button-text col-start-12';
+	actual_route: string = '';
 
+	constructor(private router: Router) {
+		this.actual_route = this.isCurrentRoute();
+
+		switch (this.actual_route) {
+			case '/home':
+				this.home_classe = 'active disabled-click text-white';
+				this.about_me_classe = 'button-text';
+				this.project_classe = 'button-text';
+				break;
+			case '/about-me':
+				this.home_classe = 'button-text';
+				this.about_me_classe = 'active disabled-click text-white';
+				this.project_classe = 'button-text';
+				break;
+			case '/project':
+				this.home_classe = 'button-text';
+				this.about_me_classe = 'button-text';
+				this.project_classe = 'active disabled-click text-white';
+				break;
+		}
+	}
+
+	isCurrentRoute(): string {
+		return this.router.url
+	}
 }
