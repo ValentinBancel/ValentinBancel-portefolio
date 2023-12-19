08039df6935ea3ab996662914e676fc90c74fe9e
valentinbancel <valentin.bancel@orange.fr>
2023-12-19 15:32:22 +0100

    add(project): add header and footer on project page

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-project',
