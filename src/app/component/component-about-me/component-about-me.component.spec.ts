8ab2d393606af594ddac59bc652e013d001a43c3
valentinbancel <valentin.bancel@orange.fr>
2023-12-19 23:44:31 +0100

    add(fix): fix tests

--- src/app/component/component-about-me/component-about-me.component.spec.ts
+++ src/app/component/component-about-me/component-about-me.component.spec.ts
@@ -1,17 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ComponentAboutMeComponent } from './component-about-me.component';
 
 describe('ComponentAboutMeComponent', () => {
-  let component: ComponentAboutMeComponent;
   let fixture: ComponentFixture<ComponentAboutMeComponent>;
-
+  let component: ComponentAboutMeComponent;
+  
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ComponentAboutMeComponent]
+      declarations: [ ComponentAboutMeComponent ]
     })
     .compileComponents();
-    
+  });
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(ComponentAboutMeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +20,19 @@ describe('ComponentAboutMeComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have history property defined', () => {
+    expect(component.history).toBeDefined();
+  });
+
+  it('should have history property initialized as empty array', () => {
+    expect(component.history).toEqual([[]]);
+  });
+
+  it('should be able to change history property', () => {
+    const newHistory = [[{ text: 'Test', image: 'Test.jpg' }]];
+    component.history = newHistory;
+    expect(component.history).toEqual(newHistory);
   });
 });
