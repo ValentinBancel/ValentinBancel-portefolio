e8399bc0cf0bd840c40c9e6038d6c615f05d91a4
valentinbancel <valentin.bancel@orange.fr>
2023-12-19 14:58:07 +0100

    add(test): create many test for all routes

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
+import { Router } from '@angular/router';
 
 describe('HeaderComponent', () => {
 	let component: HeaderComponent;
@@ -18,4 +19,34 @@ describe('HeaderComponent', () => {
 	it('should create', () => {
 		expect(component).toBeTruthy();
 	});
+	
+	  it('should return the current route', () => {
+		const router = { url: '/home' } as Router;
+		component = new HeaderComponent(router);
+		expect(component.isCurrentRoute()).toBe('/home');
+	  });
+	
+	  it('should set home_classe to active when route is /home', () => {
+		const router = { url: '/home' } as Router;
+		component = new HeaderComponent(router);
+		expect(component.home_classe).toBe('active disabled-click text-white');
+		expect(component.about_me_classe).toBe('button-text');
+		expect(component.project_classe).toBe('button-text');
+	  });
+	
+	  it('should set about_me_classe to active when route is /about-me', () => {
+		const router = { url: '/about-me' } as Router;
+		component = new HeaderComponent(router);
+		expect(component.home_classe).toBe('button-text');
+		expect(component.about_me_classe).toBe('active disabled-click text-white');
+		expect(component.project_classe).toBe('button-text');
+	  });
+	
+	  it('should set project_classe to active when route is /project', () => {
+		const router = { url: '/project' } as Router;
+		component = new HeaderComponent(router);
+		expect(component.home_classe).toBe('button-text');
+		expect(component.about_me_classe).toBe('button-text');
+		expect(component.project_classe).toBe('active disabled-click text-white');
+	  });
 });
