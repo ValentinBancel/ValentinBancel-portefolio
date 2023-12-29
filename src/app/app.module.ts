62969c15dc7a04813533e090c2a705173fd17161
valentinbancel <valentin.bancel@orange.fr>
2023-12-29 12:03:48 +0100

    add(feature): create new component, add PWA and update code for more responsive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -9,6 +9,8 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { ProjectComponent } from './project/project.component';
 import { ComponentAboutMeComponent } from './component/component-about-me/component-about-me.component';
+import { ComponentProjectCardComponent } from './component/component-project-card/component-project-card.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
 	declarations: [
@@ -19,10 +21,17 @@ import { ComponentAboutMeComponent } from './component/component-about-me/compon
 		AboutMeComponent,
 		ProjectComponent,
 		ComponentAboutMeComponent,
+		ComponentProjectCardComponent,
 	],
 	imports: [
 		BrowserModule,
 		AppRoutingModule,
+  ServiceWorkerModule.register('ngsw-worker.js', {
+    enabled: !isDevMode(),
+    // Register the ServiceWorker as soon as the application is stable
+    // or after 30 seconds (whichever comes first).
+    registrationStrategy: 'registerWhenStable:30000'
+  }),
 	],
 	providers: [],
 	bootstrap: [
---
+++ src/app/component/component-project-card/component-project-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-component-project-card',
+  templateUrl: './component-project-card.component.html',
+  styleUrl: './component-project-card.component.scss'
+})
+export class ComponentProjectCardComponent {
+
+}
