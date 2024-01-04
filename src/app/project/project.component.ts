d30c40791027750517b9ccb2bb8f1d3e2e479961
valentinbancel <valentin.bancel@orange.fr>
2024-01-04 14:04:31 +0100

    add(script): script of project page and fix component project card

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -8,5 +8,20 @@ import { ComponentProjectCardComponent } from '../component/component-project-ca
   styleUrl: './project.component.scss'
 })
 export class ProjectComponent {
-
+  public ProjectList: any = [
+    {
+      title: 'DataLink',
+      description: 'DataLink is an custom ORM for multiple database. (Is not finished yet)',
+      image: '../../../assets/Datalink_project.png',
+      link: 'https://github.com/ValentinBancel/DataLink',
+      tags: ['Python3', 'ORM', 'Database', "Neo4j", "PostgreSQL", "MySQL", "SQLite"],
+    },
+    {
+      title: 'Force de Van Der Whaal',
+      description: 'I created a c++ code for calculate the force of Van Der Whaal between two molecules.',
+      image: '../../../assets/logo_force_de_van_der_whaal.png',
+      link: 'https://github.com/ValentinBancel/Force_de_van_der_Waals',
+      tags: ["c++",'Physics', 'Van der whaal'],
+    }
+  ]
 }
