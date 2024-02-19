d6e5483a4630ba8f1b71fcf5097d46d83077376c
ValentinBancel <90621647+ValentinBancel@users.noreply.github.com>
2024-02-19 21:10:41 +0100

    add(refacto): refacto page and change responsive design

--- src/app/project/project.component.spec.ts
+++ src/app/project/project.component.spec.ts
@@ -15,26 +15,6 @@ describe('ProjectComponent', () => {
     it('should have a ProjectList property', () => {
         expect(component.ProjectList).toBeDefined();
     });
-    
-    // it('should have a title', () => {
-    // expect(component.ProjectList[0].title).toBe('DataLink');
-    // });
-
-    // it('should have a description', () => {
-    // expect(component.ProjectList[0].description).toBe('DataLink est un ORM custom pour multiple database. (Il est en cours de developpement)');
-    // });
-
-    // it('should have an image', () => {
-    // expect(component.ProjectList[0].image).toBe('assets/Datalink_project.webp');
-    // });
-
-    // it('should have a link', () => {
-    // expect(component.ProjectList[0].link).toBe('https://github.com/ValentinBancel/DataLink');
-    // });
-
-    // it('should have tags', () => {
-    // expect(component.ProjectList[0].tags).toEqual(['Python3', 'ORM', 'Database', "Neo4j", "PostgreSQL", "MySQL", "SQLite"]);
-    // });
 
     it('should have a title', () => {
         expect(component.ProjectList[0].title).toBe('Force de Van Der Whaal');
