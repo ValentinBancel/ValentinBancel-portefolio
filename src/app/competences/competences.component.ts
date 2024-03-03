940e2e8cf57aef76e8c7cfacf51ce36860d7d706
ValentinBancel <90621647+ValentinBancel@users.noreply.github.com>
2024-03-03 20:03:11 +0100

    add(lang): test fix lang component

--- src/app/competences/competences.component.ts
+++ src/app/competences/competences.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { HeaderComponent } from 'app/component/header/header.component';
+import { FooterComponent } from 'app/component/footer/footer.component';
 @Component({
   selector: 'app-competences',
   templateUrl: './competences.component.html',
